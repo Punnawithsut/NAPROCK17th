6b6e9c242c8b4844452d5a9e94b2f534b6cd9215
PunnawithSut <punnawith05@gmail.com>
2026-01-17 15:55:05 +0700

    feat: request random board from server

---
+++ bits/stdc++.h
@@ -0,0 +1,11 @@
+#include <iostream>    // cin, cout
+#include <vector>      // std::vector
+#include <string>      // std::string
+#include <algorithm>   // sort, min, max, reverse
+#include <map>         // std::map
+#include <set>         // std::set
+#include <queue>       // std::queue, std::priority_queue
+#include <stack>       // std::stack
+#include <cmath>       // floor, ceil, sqrt, abs
+#include <numeric>     // gcd, lcm, accumulate
+#include <iomanip>     // setprecision (for floating point)
